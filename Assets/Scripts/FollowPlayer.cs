192f4a3f98810db055245d5210f58585a4e835d6
Bazil Z <bazilzainal@gmail.com>
2022-01-30 22:46:46 +0800

    Init commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = player.transform.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed;
+    public float turnSpeed;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotates the vehicle
+        if (forwardInput != 0 && horizontalInput != 0)
+        {
+            transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+
+        }
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
